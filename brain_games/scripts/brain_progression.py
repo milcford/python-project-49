2a967698ad04a8fb0cf85c33707cd4727448f946
milcford <milcford@gmail.com>
2024-05-30 11:49:57 +0300

    upgrade3 brain_progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -12,7 +12,7 @@ def main():
     while count < 3:
         num1 = random.randint(1, 5) # Случайное число от 1 до 5 включительно
         num2 = random.randint(6, 50) # Случайное число от 5 до 50 включительно
-        num3 = random.randint(1, 5)
+        num3 = random.randint(2, 5)
         num_list = [str(i) for i in range(num1, num2, num3)] # Генерируем список, начальное значение
         num_list2 = num_list.copy()
         question = random.randrange(1, len(num_list2) - 2) # Число длинны списка, выбираем из него рандомное
