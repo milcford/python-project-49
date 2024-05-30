abd91700edec54891fb78f4d645d59129a1355b7
milcford <milcford@gmail.com>
2024-05-30 09:59:25 +0300

    upgrade brain_progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -10,13 +10,13 @@ def main():
     print(f'Hello, {name}')
     print('What number is missing in the progression?')
     while count < 3:
-        num1 = random.randint(1, 5)
-        num2 = random.randint(5, 50)
+        num1 = random.randint(1, 5) # Случайное число от 1 до 5 включительно
+        num2 = random.randint(5, 50) # Случайное число от 5 до 50 включительно
         num3 = random.randint(1, 5)
-        num_list = [str(i) for i in range(num1, num2, num3)]
+        num_list = [str(i) for i in range(num1, num2, num3)] # Генерируем список, начальное значение
         num_list2 = num_list.copy()
-        question = random.randrange(len(num_list2))
-        num_list2[question] = '..'
+        question = random.randrange(1, len(num_list2) - 2) # Число длинны списка, выбираем из него рандомное
+        num_list2[question] = '..' # Меняем по индексу элемент списка на две точки
         print(f'Question: {" ".join(num_list2)}')
         answer = prompt.string('Your answer: ')
         if answer == num_list[question]:
