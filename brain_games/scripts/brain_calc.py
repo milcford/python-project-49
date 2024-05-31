a00c1096d77d428b6aa81b51b602db255e20c878
milcford <milcford@gmail.com>
2024-05-31 13:15:59 +0300

    upgrade project

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,6 +2,16 @@ import random
 import prompt
 
 
+def func(result, name, count):
+    answer = prompt.string('Your answer: ')
+    if result == int(answer):
+        print('Correct!')
+        count += 1
+    else:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
+        print(f"Let's try again, {name}!")
+        break
+
 def main():
     count = 0
     print('Welcome to the Brain Games!')
@@ -16,36 +26,15 @@ def main():
         if action == '+':
             result = num1 + num2
             print(f'Question: {num1} + {num2}')
-            answer = prompt.string('Your answer: ')
-            if result == int(answer):
-                print('Correct!')
-                count += 1
-            else:
-                print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
-                print(f"Let's try again, {name}!")
-                break
-        if action == '-':
+            func(result, name, count)
+        elif action == '-':
             result = num1 - num2
             print(f'Question: {num1} - {num2}')
-            answer = prompt.string('Your answer: ')
-            if result == int(answer):
-                print('Correct!')
-                count += 1
-            else:
-                print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
-                print(f"Let's try again, {name}!")
-                break
-        if action == '*':
+            func(result, name, count)
+        elif action == '*':
             result = num1 * num2
             print(f'Question: {num1} * {num2}')
-            answer = prompt.string('Your answer: ')
-            if result == int(answer):
-                print('Correct!')
-                count += 1
-            else:
-                print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
-                print(f"Let's try again, {name}!")
-                break
+            func(result, name, count)
 
     if count == 3:
         print(f'Congratulations, {name}!')
