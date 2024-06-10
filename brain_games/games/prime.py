8b79ece372a8ace76d35d786e52dbc7732a00c75
milcford <milcford@gmail.com>
2024-06-10 12:20:32 +0300

    add prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,5 +1,7 @@
 import random
 
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
 
 def simple_num(n):
     for i in range(2, n // 2 + 1):
@@ -8,14 +10,13 @@ def simple_num(n):
     return True
 
 
-def generate_game(correct_answer, question):
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')  # Это КОНСТАНТА
+def generate_game():
     num1 = random.randint(1, 99)
     question = num1
 
-    if simple_num(num1):  # Второе условие проверяет что бы число было простым
+    if simple_num(num1):
         correct_answer = 'yes'
-    elif simple_num(num1) == False:
+    else:
         correct_answer = 'no'
     return question, correct_answer
 
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,24 +1,10 @@
-import random
+from ..engine import run_game
+from ..games import prime
 
 
-def simple_num(n):
-    for i in range(2, n // 2 + 1):
-        if n % i == 0:
-            return False
-    return True
+def main():
+    run_game(prime)
 
 
-def generate_game(correct_answer, question):
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')  # Это КОНСТАНТА
-    num1 = random.randint(1, 99)
-    question = num1
-
-    if simple_num(num1):  # Второе условие проверяет что бы число было простым
-        correct_answer = 'yes'
-    elif simple_num(num1) == False:
-        correct_answer = 'no'
-    return question, correct_answer
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
